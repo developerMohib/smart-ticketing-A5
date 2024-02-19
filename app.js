a92eddd4b359e21b1dd61fea2df98f35d606e03c
Mohib <mohibullahmohim2020@gmail.com>
2024-02-19 16:02:46 +0600

    js added more

--- app.js
+++ app.js
@@ -1,18 +1,97 @@
+/**
+ * requirement 
 
+    seat seletct kora 
+    40 tekee komano 
+    right side a 1 kore barano
+    coupon if else kora 
+    next button ok korte name number seat select kora
+
+ */
 
 function modalOpen(){
     let modal = document.getElementById('modalOpen');
     modal.classList.remove('hidden');
 }
 
-let totalSeat = document.getElementById('totlaSeat');
-let seat = parseInt(totalSeat);
+/* Bus seat count */
+let initialSeatBooked = 0;
+let initialTk = 0;
+let seatMaxBook = 4 ;
+let leftSeat = 40;
 
-console.log(typeof(seat))
+// seat excess 
 
-function getElemetTextSeat(elementId){
-    let seat = elementId.classList.add('bg-black')
-    console.log(seat)
+let totalSeat = document.getElementById('totalSeat');
+let  seatSongkha = totalSeat.innerText;
+
+console.log(seatSongkha , 'seat songkha ');
+
+
+let busSeat = document.getElementsByClassName('bus-seat')
+
+
+// Bus seat selected 
+
+
+function choiceSeatNumaberElement(elementID){
+    let playerPress = elementID.innerText;
+    console.log('Player Press : ', playerPress);
 }
-let oneSeat = document.getElementById('B3');
-getElemetTextSeat(oneSeat)
+
+
+
+
+
+for (const singleSeat of busSeat) {
+    let seatText = singleSeat.innerText;
+    console.log(seatText, 'sigle sit');
+    
+    singleSeat.addEventListener('click', function(){
+        let seatText = singleSeat.innerText;
+        console.log(seatText, 'sigle sit');
+
+        let choiceSeatName = document.getElementById('choiceSeatName');
+        choiceSeatName.innerText = seatText;
+
+        singleSeat.classList.add("bg-black");
+
+        // create container box
+
+        let containerBox = document.getElementById('seatPriceContainer');
+        let createDiv = document.createElement("div");
+
+        div.classList.add("flex", "justify-between", "my-5" );
+                div.innerHTML = `
+                    <p> ${seatText} </p>
+                    <p>Economy</p>
+                    <p>550</p>
+                `;
+                containerBox.appendChild(createDiv);
+
+        // if(singleSeat >= seatMaxBook){
+        //     singleSeat.classList.add("bg-black");
+        // }
+        // else{
+        //     alert('plase give up');
+        // }
+
+
+    });
+
+}
+
+let total = userQuantity ;
+
+let discount15 = total * 0.15 ;
+let discount20 = total * 0.15 ;
+
+if(couponInput === "new15"){
+    grandTotal = total - discount15 ;
+}
+else if(couponInput === "couple20"){
+    grandTotal = total - discount20 ;
+}
+else{
+    alert('Invalid Coupon');
+}
